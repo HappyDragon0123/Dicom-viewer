fa59407278667c940266769faacdbcad1c253c60
Semi Carry <bestmatch19940123@gmail.com>
2019-05-29 19:42:49 +1000

    django-dicom-may-29 commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '!8ked@*d!)qvam+=iyww%x1*en0+t_2o&n=ye-$d6#1#3^cx@#'
